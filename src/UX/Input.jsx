aa027d91884cd736eb5dbb2b8037c10950b7e9fc
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-16 21:02:27 -0300

    ✨ Feat: Ux, container & input

---
+++ src/UX/Input.jsx
@@ -0,0 +1,13 @@
+export default function Input({placeholder, type, value, onChange,background,color }) {
+  return (
+    <div className="mb-4">
+      <input
+      placeholder={placeholder} style={{background:background, color:color}}
+        className="w-full p-3 rounded border "
+        type={type}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+} 
